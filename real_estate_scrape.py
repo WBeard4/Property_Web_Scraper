173fd37c1427249519a2283d25a7bcf1d443e07e
WBeard4 <will6258@gmail.com>
2023-11-27 15:37:33 +0000

    added comments and tidied

--- real_estate_scrape.py
+++ real_estate_scrape.py
@@ -1,48 +1,43 @@
 from bs4 import BeautifulSoup
-import requests
 from selenium import webdriver
 
-def get_title(description):
+# Function to retrieve the description of the listing
+def get_title(description): 
     title_element = description.find('a')
     if title_element:
         title_text = title_element.text.strip()
         return title_text
 
+# Function to get the price of the listing
 def get_price(card):
     price_element = card.find('a', class_ = 'mb-0 text-lg font-bold text-denim price')
     if price_element:
-        price_text = price_element.text.strip()
-        return price_text
-    
+        price = price_element.text.strip()
+        return price
+
+# Function to get the link to the listing
 def get_link(card):
     link = card.find('a', href = True)
     link = 'https://www.onthemarket.com' + link['href']
     return link
 
+# Opens up an instance of firefox, to access the html of the page
 browser = webdriver.Firefox()
-
 browser.get('https://www.onthemarket.com/for-sale/property/gravesend/')
 page_source = browser.page_source
 soup = BeautifulSoup(page_source, 'lxml')
 
+# For each listing on the site, gets the description, price and link, then prints this information
 property_cards = soup.find_all('li', class_ ='otm-PropertyCard')
 for card in property_cards:
     description = card.find('span', class_ = 'title')
     if description:
         title_text = get_title(description)
-        if title_text:
-            price_text = get_price(card)
-            if price_text:
-                link = get_link(card)
-                print(f'{title_text}: {price_text} \n {link} \n')
-            else:
-                pass
-        else:
-            pass
+        price = get_price(card)
+        link = get_link(card)
+        print(f'{title_text}: {price} \n {link} \n')
     else:
         pass
     
-
-
-
+# Exits the browser once all the information has been collected
 browser.quit()
